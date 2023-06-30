dbd275867cbb668fea1c7fdabcb0c814bb28bc4e
bnksistemas <gpfefferman@gmail.com>
2023-06-29 22:29:41 -0300

    pantalla login

--- bases/urls.py
+++ bases/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from bases.views import Home
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),
+         name='login'),
+
 ]
--- bases/views.py
+++ bases/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class Home(generic.TemplateView):
+class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
+    login_url='/admin'
 
 
-
-
-
-   # template_name='bases/home.html' #busca en archivo raiz la carpeta template y luego otra base
 """
  esta linea estaba anteriormente y nos permía direccionar el archivo padre ubicado en la carpeta
  template del proyecto (la plantilla hija esta ubicada en la carpeta template de la aplicacion)
