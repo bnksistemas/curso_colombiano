bdea0a8bb5f5380df1e86c4ea16a69376a2978a0
bnksistemas <gpfefferman@gmail.com>
2023-07-04 17:42:54 -0300

    login y antes de logout

--- bases/views.py
+++ bases/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
-    login_url='/admin'
-
-
-"""
- esta linea estaba anteriormente y nos permía direccionar el archivo padre ubicado en la carpeta
- template del proyecto (la plantilla hija esta ubicada en la carpeta template de la aplicacion)
-
-    template_name='base/base.html' #busca en archivo raiz la carpeta template y luego otra base
-"""
+    login_url='bases:login'
--- manage.py
+++ manage.py
@@ -2,11 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#prbando repositorio
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
         from django.core.management import execute_from_command_line
